ca14e1b5ff6888aebdb09818923619d7799a5c6e
SameerGiri69 <sameergiri153000@gmail.com>
2023-11-02 12:07:50 +0545

    creating models

---
+++ WEBSITE101/Model/Pokemons.cs
@@ -0,0 +1,9 @@
+﻿namespace WEBSITE101.Model
+{
+    public class Pokemons
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
